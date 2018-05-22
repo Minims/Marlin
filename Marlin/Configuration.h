e32273d9e8671f256bea40dea2d3dbbd2e96b3fb
ngandalf <nicolas.thomas@dreaminformatique.com>
2018-05-22 10:46:52 +0200

    Update Configuration.h
    fix power supply and baudrate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -111,7 +111,7 @@
  *
  * :[2400, 9600, 19200, 38400, 57600, 115200, 250000, 500000, 1000000]
  */
-#define BAUDRATE 250000
+#define BAUDRATE 115200
 
 // Enable the Bluetooth serial interface on AT90USB devices
 //#define BLUETOOTH
@@ -226,7 +226,7 @@
  *
  * :{ 0:'No power switch', 1:'ATX', 2:'X-Box 360' }
  */
-#define POWER_SUPPLY 2
+#define POWER_SUPPLY 1
 
 #if POWER_SUPPLY > 0
   // Enable this option to leave the PSU off at startup.
