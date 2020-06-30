e56992c0b4ca2920fd69c051a17875c820f8d6ba
Minims <github@minims.fr>
2020-06-30 21:28:17 +0200

    [SKR PRO] Setup Extruder PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -482,17 +482,17 @@
 #define PID_K1 0.95      // Smoothing factor within any PID loop
 
 #if ENABLED(PIDTEMP)
-  //#define PID_EDIT_MENU         // Add PID editing to the "Advanced Settings" menu. (~700 bytes of PROGMEM)
-  //#define PID_AUTOTUNE_MENU     // Add PID auto-tuning to the "Advanced Settings" menu. (~250 bytes of PROGMEM)
+  #define PID_EDIT_MENU         // Add PID editing to the "Advanced Settings" menu. (~700 bytes of PROGMEM)
+  #define PID_AUTOTUNE_MENU     // Add PID auto-tuning to the "Advanced Settings" menu. (~250 bytes of PROGMEM)
   //#define PID_PARAMS_PER_HOTEND // Uses separate PID parameters for each extruder (useful for mismatched extruders)
                                   // Set/get with gcode: M301 E[extruder number, 0-2]
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
-  // Ultimaker
-  #define DEFAULT_Kp 22.2
-  #define DEFAULT_Ki 1.08
-  #define DEFAULT_Kd 114
+  // E3D V6 Clone with Silicone 50W - 240°C
+  #define DEFAULT_Kp 12.86
+  #define DEFAULT_Ki 0.68
+  #define DEFAULT_Kd 60.58
 
   // MakerGear
   //#define DEFAULT_Kp 7.0
