d32a3f41c97ac5a1b26db1cac4867de3316b493c
Minims <github@minims.fr>
2020-06-30 20:32:38 +0200

    [SKR PRO] Setup PS_ON

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -326,8 +326,8 @@
  * Enable and connect the power supply to the PS_ON_PIN.
  * Specify whether the power supply is active HIGH or active LOW.
  */
-//#define PSU_CONTROL
-//#define PSU_NAME "Power Supply"
+#define PSU_CONTROL
+#define PSU_NAME "360W 12V"
 
 #if ENABLED(PSU_CONTROL)
   #define PSU_ACTIVE_HIGH false     // Set 'false' for ATX, 'true' for X-Box
