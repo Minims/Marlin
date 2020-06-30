6ac65419aca27cc507f6036fed17306554e4c49c
Minims <github@minims.fr>
2020-06-30 21:29:28 +0200

    [SKR PRO] Setup Bed PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -523,7 +523,7 @@
  * heater. If your configuration is significantly different than this and you don't understand
  * the issues involved, don't use bed PID until someone else verifies that your hardware works.
  */
-//#define PIDTEMPBED
+#define PIDTEMPBED
 
 //#define BED_LIMIT_SWITCHING
 
@@ -539,11 +539,10 @@
   //#define MIN_BED_POWER 0
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
-  //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
-  //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  // CR-10 Bed Tuned for 95C insulated.
+  #define DEFAULT_bedKp 212.51
+  #define DEFAULT_bedKi 41.84
+  #define DEFAULT_bedKd 719.56
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
